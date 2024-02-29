a772af53a61f616ba08fd08383e932ad88cb841d
Tedd OKANO <okano.akifumi@gmail.com>
2024-03-01 04:12:22 +0900

    reg_dump feature added

--- source/i3c_master_read_sensor_p3t1755.c
+++ source/i3c_master_read_sensor_p3t1755.c
@@ -38,10 +38,13 @@
  ******************************************************************************/
 void		init_MCU( void );
 void		init_I3C( void );
+float		temp_sensor_setting( uint8_t addr, uint8_t config );
+void		temp_sensor_reg_dump( uint8_t addr );
 float		read_temp( uint8_t targ, uint8_t reg );
 void		write_temp( uint8_t targ, uint8_t reg, float v );
 float		short2celsius( uint16_t v );
 uint16_t	celsius2short( float v );
+uint16_t	swap_bytes( uint16_t v );
 
 /*******************************************************************************
  * Variables
@@ -61,45 +64,60 @@ int main(void)
 	//	Try DAA
 	i3c_change_target_address( P3T1755_ADDR_I2C, P3T1755_ADDR_I3C << 1 );
 
-#ifdef TRY_IBI
 	float	temp;
-	uint8_t	config	= P3T1755_CONFIG_VALUE;
+	temp	= temp_sensor_setting( P3T1755_ADDR_I3C, P3T1755_CONFIG_VALUE );
+	PRINTF( "  T_HIGH and T_LOW registers are set based on current temperature: %8.4f\r\n", temp );
 	
-	i3c_reg_write( P3T1755_ADDR_I3C, P3T1755_REG_Conf, &config, sizeof( config ) );
-	i3c_reg_read(  P3T1755_ADDR_I3C, P3T1755_REG_Conf, &config, sizeof( config ) );
-	PRINTF( "config:0x%02X\r\n", config );
-	
-	i3c_enable_IBI( P3T1755_ADDR_I3C );
-
-	temp	= read_temp( P3T1755_ADDR_I3C, P3T1755_REG_Temp );
-	PRINTF( " Temp reg read value: %8.4f\r\n", temp );
-	
-	write_temp( P3T1755_ADDR_I3C, P3T1755_REG_T_HIGH, temp + 2.0 );
-	write_temp( P3T1755_ADDR_I3C, P3T1755_REG_T_LOW,  temp + 1.0 );
-	
-	temp	= read_temp( P3T1755_ADDR_I3C, P3T1755_REG_T_HIGH );
-	PRINTF( " T_HIGH(0x03) = %8.4f\r\n", temp );
-
-	temp	= read_temp( P3T1755_ADDR_I3C, P3T1755_REG_T_LOW  );
-	PRINTF( " T_LOW (0x02) = %8.4f\r\n", temp );	
+	temp_sensor_reg_dump( P3T1755_ADDR_I3C );
 
 	uint8_t	ibi_addr;
 	
-#endif // TRY_IBI
-
 	while (1)
 	{
-#ifdef TRY_IBI
 		if ( (ibi_addr	= i3c_check_IBI()) )
-		{
 			PRINTF("*** IBI : Got IBI from target_address: 7’h%02X (0x%02X)\n", ibi_addr, ibi_addr << 1 );
-		}
-#endif // TRY_IBI
+
 		temp	= read_temp( P3T1755_ADDR_I3C, P3T1755_REG_Temp );
 
-		PRINTF( "Temperature: %8.4f \r\n", temp );
+		PRINTF( "Temperature: %8.4f˚C\r\n", temp );
 		SDK_DelayAtLeastUs(1000000, CLOCK_GetCoreSysClkFreq());
 	}
+}
+
+float temp_sensor_setting( uint8_t addr, uint8_t config )
+{
+	float	temp, high, low;
+	
+	i3c_reg_write( addr, P3T1755_REG_Conf, &config, sizeof( config ) );
+
+	temp	= read_temp( addr, P3T1755_REG_Temp );
+	high	= temp + 2.0;
+	low		= temp + 1.0;
+	
+	write_temp( addr, P3T1755_REG_T_HIGH, high );
+	write_temp( addr, P3T1755_REG_T_LOW,  low  );
+
+	i3c_enable_IBI( addr );
+	
+	return temp;
+}
+
+void temp_sensor_reg_dump( uint8_t addr )
+{
+	uint16_t	t, h, l;
+	uint8_t		c;
+	
+	i3c_reg_read( addr, P3T1755_REG_Temp,   &t, sizeof( t ) );
+	i3c_reg_read( addr, P3T1755_REG_Conf,   &c, sizeof( c ) );
+	i3c_reg_read( addr, P3T1755_REG_T_LOW,  &h, sizeof( h ) );
+	i3c_reg_read( addr, P3T1755_REG_T_HIGH, &l, sizeof( l ) );
+
+	PRINTF( "\r\n  P3T1755 (I3C target address:7’h%02X (0x%02X)) register dump\r\n", P3T1755_ADDR_I3C, P3T1755_ADDR_I3C << 1 );	
+	PRINTF( "  * Temp   (0x0): 0x%04X (%8.4f˚C)\r\n", swap_bytes( t ), short2celsius( t ) );	
+	PRINTF( "  * Conf   (0x1): 0x  %02X\r\n", c );	
+	PRINTF( "  * T_LOW  (0x2): 0x%04X (%8.4f˚C)\r\n", swap_bytes( l ), short2celsius( l ) );	
+	PRINTF( "  * T_HIGH (0x3): 0x%04X (%8.4f˚C)\r\n", swap_bytes( h ), short2celsius( h ) );	
+	PRINTF( "\r\n" );	
 }
 
 float read_temp( uint8_t targ, uint8_t reg )
@@ -118,21 +136,21 @@ void write_temp( uint8_t targ, uint8_t reg, float v )
 
 float short2celsius( uint16_t v )
 {
-#ifdef __BIG_ENDIAN__
-	return (float)v * 0.0625;
-#else
-	return (float)(((v & 0x00FF) << 4) | ((v & 0xFF00) >> 12)) * 0.0625;
-#endif
+	static const float	k	= 1 / 256.0;
+	return (float)(swap_bytes( v )) * k;
 }
 
 uint16_t celsius2short( float v )
 {
-	uint16_t tmp	= (uint16_t)(v * 256.0);
+	return swap_bytes( (uint16_t)(v * 256.0) );
+}
 
+uint16_t swap_bytes( uint16_t v )
+{
 #ifdef __BIG_ENDIAN__
-	return tmp;
+	return v;
 #else	
-	return (uint16_t)((tmp << 8) | (tmp >> 8));
+	return (v << 8) | (v >> 8);
 #endif
 }
 
